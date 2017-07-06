0561eb002071b805d8a2389b992d8f60918a0582
hiwooj <hiwooj35@gmail.com>
2017-07-06 16:52:37 +0900

    공지사항 및 자료실 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -2,10 +2,10 @@ $(function() {
     // 메인 메뉴 제어를 위한 스크립트
     $(".main-menu > li").hover(
         function() {
-            $(this).siblings("ul").css("display", "block");
+            $(this).find("ul").css("display", "block");
         },
         function() {
-            $(this).siblings("ul").css("display", "none");
+            $(this).find("ul").css("display", "none");
         }
     );
     $(".main-menu span").focusin(
@@ -13,16 +13,9 @@ $(function() {
             $(this).siblings("ul").css("display", "block");
         }
     );
-    $(".main-menu ul li:last-child a").focusout(
-        function() {
-            $(this).parent().parent().css("display", "none");
-        }
-    );
-});
 
-$(function() {
     // 탭메뉴 제어를 위한 자바스크립트
-    $(".board h2").click(function() {
+    $(".board h2").focus(function() {
         $(this).parent().addClass("act")
             .siblings().removeClass("act");
     });
