5dd8ba6798038a11b8c7936042a15dd826d3f84a
hiwooj <hiwooj35@gmail.com>
2017-07-06 16:20:48 +0900

    공지사항 및 자료실 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,5 +1,5 @@
-// 서브메뉴 제어를 위한 스크립트
 $(function() {
+    // 메인 메뉴 제어를 위한 스크립트
     $(".main-menu > li").hover(
         function() {
             $(this).siblings("ul").css("display", "block");
@@ -18,4 +18,12 @@ $(function() {
             $(this).parent().parent().css("display", "none");
         }
     );
+});
+
+$(function() {
+    // 탭메뉴 제어를 위한 자바스크립트
+    $(".board h2").click(function() {
+        $(this).parent().addClass("act")
+            .siblings().removeClass("act");
+    });
 });
